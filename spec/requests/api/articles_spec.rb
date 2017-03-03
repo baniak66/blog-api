ad4894622f8ac3ac2e56d8eba7d320c2b3a194b1
Pawel Licznar <pawellicznar@gmail.com>
2017-03-04 00:13:00 +0100

    Refactor requests specs

--- spec/requests/api/articles_spec.rb
+++ spec/requests/api/articles_spec.rb
@@ -3,32 +3,32 @@ include Rack::Test::Methods
 
 RSpec.describe "Article API", :type => :request do
 
-  before(:each) do
-    @user = FactoryGirl.create(:user)
-  end
+  let(:user) { create :user }
+  let(:article) { create :article }
 
   describe 'anonymous user' do
     it "retrieves a specific article" do
-      @article = FactoryGirl.create(:article)
-      get "/articles/#{@article.id}"
+      get "/articles/#{article.id}"
 
       expect(response).to be_success
-      expect(json['title']).to eq(@article.title)
+      expect(json['title']).to eq(article.title)
     end
 
-    it 'sends a list of articles' do
-      FactoryGirl.create_list(:article, 10)
-      get '/articles'
+    context 'retrives list' do
+      let!(:articles) { create_list :article, 10 }
+      it 'of articles' do
+        get '/articles'
 
-      expect(response).to be_success
-      expect(json.length).to eq(10)
+        expect(response).to be_success
+        expect(json.length).to eq(10)
+      end
     end
 
     it "can't crete an article" do
       expect(Article.all.length).to eq(0)
       post "/articles/",
         :params => {:article => { :title => "new title",
-                      :content => "new content" }, :email => @user.email}
+                      :content => "new content" }, :email => user.email}
 
       expect(response).to have_http_status(401)
       expect(Article.all.length).to eq(0)
@@ -41,8 +41,8 @@ RSpec.describe "Article API", :type => :request do
       expect(Article.all.length).to eq(0)
       post "/articles/",
         :params => {:article => { :title => "new title",
-                      :content => "new content" }, :email => @user.email},
-        :headers => {'Authorization' => get_token(@user)}
+                      :content => "new content" }, :email => user.email},
+        :headers => {'Authorization' => get_token(user)}
 
       expect(response).to have_http_status(201)
       expect(Article.all.length).to eq(1)
@@ -50,44 +50,52 @@ RSpec.describe "Article API", :type => :request do
       expect(json['content']).to eq("new content")
     end
 
-    it 'deletes an article' do
-      @article = FactoryGirl.create(:article, user_id: @user.id)
-      expect(Article.all.length).to eq(1)
-      delete "/articles/#{@article.id}", :headers => {'Authorization' => get_token(@user)}
+    context 'author' do
+      let!(:article) { create :article, user_id: user.id }
+      it 'deletes article' do
+        expect(Article.all.length).to eq(1)
+        delete "/articles/#{article.id}", :headers => {'Authorization' => get_token(user)}
 
-      expect(response).to be_success
-      expect(Article.all.length).to eq(0)
+        expect(response).to be_success
+        expect(Article.all.length).to eq(0)
+      end
     end
 
-    it "can't delete article if not author" do
-      @article = FactoryGirl.create(:article)
-      expect(Article.all.length).to eq(1)
-      delete "/articles/#{@article.id}", :headers => {'Authorization' => get_token(@user)}
+    context 'not author' do
+      let!(:article) { create :article }
+      it "can't delete article if not author" do
+        expect(Article.all.length).to eq(1)
+        delete "/articles/#{article.id}", :headers => {'Authorization' => get_token(user)}
 
-      expect(response).to have_http_status(422)
-      expect(Article.all.length).to eq(1)
+        expect(response).to have_http_status(422)
+        expect(Article.all.length).to eq(1)
+      end
     end
 
-    it 'updates an article by author' do
-      @article = FactoryGirl.create(:article, user_id: @user.id)
-      put "/articles/#{@article.id}", :params => {:id => @article.id,
-        :article => { :title => "title updated",
-                      :content => "content updated" }},
-        :headers => {'Authorization' => get_token(@user)}
+    context 'author' do
+      let!(:article) { create :article, user_id: user.id }
+      it 'updates an article by author' do
+        put "/articles/#{article.id}", :params => {:id => article.id,
+          :article => { :title => "title updated",
+                        :content => "content updated" }},
+          :headers => {'Authorization' => get_token(user)}
 
-      expect(response).to have_http_status(201)
-      expect(json['title']).to eq("title updated")
-      expect(json['content']).to eq("content updated")
+        expect(response).to have_http_status(201)
+        expect(json['title']).to eq("title updated")
+        expect(json['content']).to eq("content updated")
+      end
     end
 
-    it "can't update article it not author" do
-      @article = FactoryGirl.create(:article)
-      put "/articles/#{@article.id}", :params => {:id => @article.id,
-        :article => { :title => "title updated",
-                      :content => "content updated" }},
-        :headers => {'Authorization' => get_token(@user)}
+    context 'not author' do
+      let!(:article) { create :article }
+      it "can't update article it not author" do
+        put "/articles/#{article.id}", :params => {:id => article.id,
+          :article => { :title => "title updated",
+                        :content => "content updated" }},
+          :headers => {'Authorization' => get_token(user)}
 
-      expect(response).to have_http_status(422)
+        expect(response).to have_http_status(422)
+      end
     end
 
   end
--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -3,34 +3,31 @@ include Rack::Test::Methods
 
 RSpec.describe "Comment API", :type => :request do
 
-  before(:each) do
-    @user = FactoryGirl.create(:user)
-  end
+  let(:user) { create :user }
+  let(:article) { create :article }
 
   describe "anonymous user" do
     it "can't add comment" do
-      @article = FactoryGirl.create(:article)
-      expect(@article.comments.all.length).to eq(0)
-      post "/articles/#{@article.id}/comments",
+      expect(article.comments.all.length).to eq(0)
+      post "/articles/#{article.id}/comments",
           :params => {:comment => { :content => "new content" },
-                      :email => @user.email}
+                      :email => user.email}
 
       expect(response).to have_http_status(401)
-      expect(@article.comments.all.length).to eq(0)
+      expect(article.comments.all.length).to eq(0)
     end
   end
 
   describe "authenticated user" do
     it "creates a comment" do
-      @article = FactoryGirl.create(:article)
-      expect(@article.comments.all.length).to eq(0)
-      post "/articles/#{@article.id}/comments",
+      expect(article.comments.all.length).to eq(0)
+      post "/articles/#{article.id}/comments",
           :params => {:comment => { :content => "new content" },
-                      :email => @user.email},
-          :headers => {'Authorization' => get_token(@user)}
+                      :email => user.email},
+          :headers => {'Authorization' => get_token(user)}
 
       expect(response).to have_http_status(201)
-      expect(@article.comments.all.length).to eq(1)
+      expect(article.comments.all.length).to eq(1)
     end
   end
 end
